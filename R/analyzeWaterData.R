44a031de29bad6104e21b3fd9e316d8a7f8687b7
Prashant Bhandari <prashantbhandarinepal@gmail.com>
2023-07-21 10:34:03 -0500

    applied fix on the previous iters

--- R/analyzeWaterData.R
+++ R/analyzeWaterData.R
@@ -57,54 +57,49 @@ analyzeWaterData <- function(water_data, predicted_pcv_data, project_code) {
   }
 
   # Calculate water amount lost
-  water_df <- water_df %>%
-    filter(weight.before > -1) %>%
+  water_df <- water_df %>% filter(weight.before > -1) %>%
     filter(weight.after > -1) %>%
-    group_by(plantbarcode, day, genotype, treatment, replicate) %>%
-    arrange(plantbarcode, day, genotype, treatment, replicate) %>%
-    mutate(weight.after.lag1 = lag(weight.after, 1)) %>%
-    mutate(water.amount.plus = weight.after.lag1 - weight.before) %>%
+    group_by(plantbarcode) %>%
+    arrange(plantbarcode, day, timestamp) %>%
+    mutate(weight.after.lag1 = lag(weight.after,1)) %>%
+    mutate(water.amount.plus = weight.after.lag1 -  weight.before) %>%
     filter(!is.na(water.amount.plus)) %>%
-    group_by(plantbarcode, day) %>%
-    mutate(water_amount_plus_dap = sum(water.amount.plus, na.rm = TRUE))
+    group_by(plantbarcode) %>%
+    mutate(time.water.loss = as_datetime(timestamp) -
+             as_datetime(lag(timestamp, 1))) %>%
+    group_by(plantbarcode, day) %>% arrange(timestamp) %>%
+    mutate(watering.job.num = 1:n()) %>%
+    arrange(plantbarcode, day, watering.job.num) %>%
+    group_by(plantbarcode, day, genotype, treatment, replicate) %>%
+    summarize(water.amount.plus.dap = sum(water.amount.plus), .groups = "drop")
 
-  water.join <- water_df %>%
-    select(plantbarcode, genotype, treatment, replicate, water.amount, water.amount.plus, day)
-
-  # Modify sv2 dataframe
-  sv2 <- predicted_pcv_data %>%
-    mutate(day = 12 + DAP) %>%
+  water.join <- water_df %>% select(plantbarcode, genotype, day,
+                                    treatment, replicate, water.amount.plus.dap)
+  sv2 <- predicted_pcv_data %>% mutate(day = 12 + DAP) %>%
     select(-c(genotype, treatment, DAP))
-
-  joined1 <- left_join(water.join, sv2, by = c("plantbarcode", "day"))
+  joined1 <- left_join(water.join, sv2, by = c("plantbarcode",
+                                               "day"))
   joined1 <- distinct(joined1)
   joined1 <- joined1 %>% filter(day != 12)
-  joined1 <- joined1 %>% filter(water.amount.plus > 0)
+  joined1 <- joined1 %>% filter(water.amount.plus.dap > 0)
   joined1 <- drop_na(joined1)
-  joined1$dailyWUE <- joined1$daily_area_diff_pred / joined1$water.amount.plus
-
+  joined1$dailyWUE <- joined1$daily_area_diff_pred/joined1$water.amount.plus.dap
   num_na <- sum(is.na(joined1$dailyWUE))
   if (num_na > 0) {
     cat("There are", num_na, "NA values in joined1$dailyWUE.\n")
-  } else {
+  }
+  else {
     print("No NA values in joined1$dailyWUE.")
   }
-
-  wue.fit.models <- joined1 %>%
-    group_by(day) %>%
-    do(mod = lm(daily_area_diff_pred ~ water.amount.plus, data = .))
-
+  wue.fit.models <- joined1 %>% group_by(day) %>% do(mod = lm(daily_area_diff_pred ~
+                                                                water.amount.plus.dap, data = .))
   m <- left_join(joined1, wue.fit.models, by = c("day"))
-
-  x <- m %>%
-    group_by(day) %>%
-    do(add_predictions(., first(.$mod), var = 'WUE.fit')) %>%
-    do(add_residuals(., first(.$mod), var = 'WUE.resid')) %>%
+  x <- m %>% group_by(day) %>%
+    do(add_predictions(., first(.$mod), var = "WUE.fit")) %>%
+    do(add_residuals(., first(.$mod), var = "WUE.resid")) %>%
     select(-c(mod))
-
-  output_file <- paste(project_code, "large_plot_file.csv", sep = "_")
+  output_file <- paste(project_code, "large_plot_file.csv",
+                       sep = "_")
   write.csv(x, file = output_file, row.names = FALSE)
-
-  # Return the final data frame
   return(x)
 }
