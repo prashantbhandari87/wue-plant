1b4eee5bf1e245a0539f367d0061576181f14baa
Prashant Bhandari <prashantbhandarinepal@gmail.com>
2023-07-25 11:53:55 -0500

    finally solved the daily area diff issue

--- R/runGWASAnalysisEfficient2.R
+++ R/runGWASAnalysisEfficient2.R
@@ -80,7 +80,8 @@ runGWASAnalysisEfficient2 <- function (bedfile, famFile, phenoFile, phenoCols, p
     out <- as.data.frame(cbind(CHR, POS, obj.gwas.gc))
     out <- out %>% filter(-log10(score) > 5)
     write.csv(out, res_file, row.names = FALSE)
-    unlink(paste0(tmpfile, "*"))
+    gzip(out)
+    file.remove(out)
   }
   print("Files written out!")
 }
